d4d7946dfa46382e177812a796d98945d8298d46
ctj <chentingjian@face-future.com>
2021-10-22 15:50:49 +0800

    修改ping规则,增加多个dns备用

--- xlink/src/main/java/com/future/xlink/utils/PingUtils.java
+++ xlink/src/main/java/com/future/xlink/utils/PingUtils.java
@@ -12,7 +12,11 @@ import java.io.InputStreamReader;
  */
 public class PingUtils {
     private static final String TAG = "PingUtils";
-
+    public static final String[] PING_ADDR = new String[]{
+            "114.114.114.114", "223.5.5.5", "223.6.6.6", "180.76.76.76", "8.8.8.8",
+            "114.114.115.115", "119.29.29.29", "210.2.4.8", "9.9.9.9", "199.91.73.222",
+            "101.226.4.6","1.2.4.8"
+    };
     public static String ping(String host, int pingCount, StringBuffer stringBuffer) {
         String result = null;
         String line = null;
@@ -100,16 +104,44 @@ public class PingUtils {
         }
         return false;
     }
-
+    /**
+     * 两重循环去重
+     * 随机指定范围内N个不重复的数
+     * 最简单最基本的方法
+     * @param min 指定范围最小值
+     * @param max 指定范围最大值
+     * @param randomSize 随机数个数
+     */
+    public static int[] randomCommon(int min, int max, int randomSize) {
+        if (randomSize > (max - min + 1) || max < min) {
+            return null;
+        }
+        int[] result = new int[randomSize];
+        int count = 0;
+        while (count < randomSize) {
+            int num = (int) (Math.random() * (max - min)) + min;
+            boolean flag = true;
+            for (int j = 0; j < randomSize; j++) {
+                if (num == result[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                result[count] = num;
+                count++;
+            }
+        }
+        return result;
+    }
     /**
      * 判断网络是否异常
      */
     public static boolean checkNetWork() {
-        String[] PING_ADDR = new String[]{
-                "114.114.114.114", "223.5.5.5", "180.76.76.76", "8.8.8.8"
-        };
-        for (int i = 0; i < PING_ADDR.length; i++) {
-            boolean isNetOk = ping(PING_ADDR[i], 1, 2);
+        int [] randomArray=randomCommon(0,PING_ADDR.length-1,3);
+        for (int i = 0; i < randomArray.length; i++) {
+            String pingAddr=PING_ADDR[randomArray[i]];
+            boolean isNetOk = ping(pingAddr, 1, 2);
             if (!isNetOk) {
                 //If it is abnormal when entering the program network at the beginning, then only prompt once
                 continue;

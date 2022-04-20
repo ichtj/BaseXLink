f58699653338ddb271d990b6d4083f1c93b968cf
ctj <chentingjian@face-future.com>
2022-04-20 17:25:23 +0800

    优化获取产品信息的接口类定义

--- xlink/src/main/java/com/future/xlink/bean/ProductInfo.java
+++ xlink/src/main/java/com/future/xlink/bean/ProductInfo.java
@@ -3,15 +3,45 @@ package com.future.xlink.bean;
 public class ProductInfo {
     private String deviceSn;
     private int productId;
+    private int officeId;
+    private String productName;
+    private String officeName;
     private String activationDate;
 
     public ProductInfo() {
     }
 
-    public ProductInfo(String deviceSn, int productId, String activationDate) {
+    public ProductInfo(String deviceSn, int productId, int officeId, String productName, String officeName, String activationDate) {
         this.deviceSn = deviceSn;
         this.productId = productId;
+        this.officeId = officeId;
+        this.productName = productName;
+        this.officeName = officeName;
         this.activationDate = activationDate;
+    }
+
+    public int getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(int officeId) {
+        this.officeId = officeId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getOfficeName() {
+        return officeName;
+    }
+
+    public void setOfficeName(String officeName) {
+        this.officeName = officeName;
     }
 
     public String getDeviceSn() {
@@ -36,5 +66,17 @@ public class ProductInfo {
 
     public void setActivationDate(String activationDate) {
         this.activationDate = activationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductInfo{" +
+                "deviceSn='" + deviceSn + '\'' +
+                ", productId=" + productId +
+                ", officeId=" + officeId +
+                ", productName='" + productName + '\'' +
+                ", officeName='" + officeName + '\'' +
+                ", activationDate='" + activationDate + '\'' +
+                '}';
     }
 }

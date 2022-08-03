988ea827ade168a0cf81b013fc1b949b16fc7325
ctj <chentingjian@face-future.com>
2022-08-03 11:06:09 +0800

    延长超时时间

--- xlink/src/main/java/com/future/xlink/api/retrofit/OkHttpSingleton.java
+++ xlink/src/main/java/com/future/xlink/api/retrofit/OkHttpSingleton.java
@@ -20,9 +20,9 @@ import okio.Buffer;
 
 final class OkHttpSingleton {
     private static final String TAG = "OkHttpSingleton";
-    private static final long CONNECT_TIMEOUT = 10;
-    private static final long WRITE_TIMEOUT = 10;
-    private static final long READ_TIMEOUT = 10;
+    private static final long CONNECT_TIMEOUT = 20;
+    private static final long WRITE_TIMEOUT = 20;
+    private static final long READ_TIMEOUT = 20;
 
     private OkHttpSingleton() {
     }

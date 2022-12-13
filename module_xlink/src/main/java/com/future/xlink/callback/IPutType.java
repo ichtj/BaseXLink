191d6fc7573b4c72a651645d88ecd0ff95948a67
ctj <chentingjian@face-future.com>
2022-12-13 11:58:08 +0800

    添加指令对upgrade支持

--- module_xlink/src/main/java/com/future/xlink/callback/IPutType.java
+++ module_xlink/src/main/java/com/future/xlink/callback/IPutType.java
@@ -7,7 +7,7 @@ import com.future.xlink.bean.PutType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@IntDef({PutType.EVENT,PutType.METHOD,PutType.SETPERTIES,PutType.GETPERTIES,PutType.UPLOAD})
+@IntDef({PutType.EVENT,PutType.METHOD,PutType.UPGRADE,PutType.SETPERTIES,PutType.GETPERTIES,PutType.UPLOAD})
 @Retention(RetentionPolicy.SOURCE)
 public @interface IPutType {
 }
